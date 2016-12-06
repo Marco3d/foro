86e41f378132b8a1a7360def59df4a0c412b3175
Marco <marcoperafan@gmail.com>
2016-12-06 01:54:50 -0500

    pruebas unitarias e integracion

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -11,6 +13,12 @@ class Post extends Model
      public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+     public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 
 }
